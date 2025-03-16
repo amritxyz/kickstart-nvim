95c87b7db20d098c55525b01a0e9657853845e21
amritxyz <amrit44404@proton.me>
2025-03-16 12:28:21 +0545

    gruvbox->catppuccin

--- init.lua
+++ init.lua
@@ -992,23 +992,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'cpp',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-        'java',
-        'json',
-        'ruby',
-      },
+      ensure_installed = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'java', 'json', 'ruby', },
       -- Autoinstall languages that are not installed
       auto_install = false,
       highlight = {
@@ -1016,23 +1000,7 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = {
-          'bash',
-          'c',
-          'cpp',
-          'diff',
-          'html',
-          'lua',
-          'luadoc',
-          'markdown',
-          'markdown_inline',
-          'query',
-          'vim',
-          'vimdoc',
-          'java',
-          'json',
-          'ruby',
-        },
+        additional_vim_regex_highlighting = { 'bash', 'c', 'cpp', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'java', 'json', 'ruby', },
       },
       indent = {
         enable = true,
