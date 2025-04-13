e3732cd7b206a2fc4ae62d349f93df69b002d5cd
amritxyz <amrit44404@proton.me>
2025-04-13 19:25:33 +0545

    lua/config/keybind.lua: spellcheck toggle

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -1,4 +1,5 @@
 -- Keymaps
+vim.keymap.set('n', '<C-s>', '<cmd>set spell!<CR>')
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
