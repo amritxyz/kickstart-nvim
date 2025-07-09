4157f392565eed78d73a354be91bf5224524e55d
amritxyz <amrit44404@proton.me>
2025-07-09 18:39:40 +0545

    just updated plugins

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -4,5 +4,7 @@ return {
   init = function()
     vim.cmd.colorscheme 'gruvbox-material'
     vim.cmd.hi 'Comment gui=none'
+    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   end,
 }
