0f8e1adbbb5117690185c9c4fec039d0db9dd9e8
amritxyz <amrit44404@proton.me>
2025-08-19 23:14:00 +0545

    chore: some minor scheme change

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,8 +1,31 @@
 return {
-  'sainnhe/gruvbox-material',
+  'ellisonleao/gruvbox.nvim',
   priority = 1000,
   init = function()
-    vim.cmd.colorscheme 'gruvbox-material'
+    require('gruvbox').setup {
+      terminal_colors = true,
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = true,
+        emphasis = true,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      inverse = true,
+      contrast = 'soft',
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = false,
+    }
+    vim.cmd.colorscheme 'gruvbox'
     vim.cmd.hi 'Comment gui=none'
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none', fg = '#ebdbb2' })
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -13,10 +13,10 @@ return {
     statusline.section_location = function()
       return '%2l:%-2v'
     end
-    vim.api.nvim_set_hl(0, 'MiniStatuslineModeNormal', { fg = '#1d2021', bg = '#a89984', bold = false })
-    vim.api.nvim_set_hl(0, 'MiniStatuslineModeInsert', { fg = '#1d2021', bg = '#a9b665', bold = false })
-    vim.api.nvim_set_hl(0, 'MiniStatuslineModeVisual', { fg = '#1d2021', bg = '#ea6962', bold = false })
-    vim.api.nvim_set_hl(0, 'MiniStatuslineModeReplace', { fg = '#1d2021', bg = '#d8a657', bold = false })
-    vim.api.nvim_set_hl(0, 'MiniStatuslineModeCommand', { fg = '#1d2021', bg = '#7daea3', bold = false })
+    -- vim.api.nvim_set_hl(0, 'MiniStatuslineModeNormal', { fg = '#1d2021', bg = '#a89984', bold = false })
+    -- vim.api.nvim_set_hl(0, 'MiniStatuslineModeInsert', { fg = '#1d2021', bg = '#a9b665', bold = false })
+    -- vim.api.nvim_set_hl(0, 'MiniStatuslineModeVisual', { fg = '#1d2021', bg = '#ea6962', bold = false })
+    -- vim.api.nvim_set_hl(0, 'MiniStatuslineModeReplace', { fg = '#1d2021', bg = '#d8a657', bold = false })
+    -- vim.api.nvim_set_hl(0, 'MiniStatuslineModeCommand', { fg = '#1d2021', bg = '#7daea3', bold = false })
   end,
 }
