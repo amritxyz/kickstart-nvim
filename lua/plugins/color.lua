1c8325572a442e17cca1773c2b8e5a0f04865a0a
amritxyz <amrit44404@proton.me>
2025-08-22 17:36:42 +0545

    chore: scheme change

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,33 +1,10 @@
 return {
-  'ellisonleao/gruvbox.nvim',
+  'idr4n/github-monochrome.nvim',
   priority = 1000,
-  init = function()
-    require('gruvbox').setup {
-      terminal_colors = true,
-      undercurl = true,
-      underline = true,
-      bold = true,
-      italic = {
-        strings = true,
-        emphasis = true,
-        comments = true,
-        operators = false,
-        folds = true,
-      },
-      strikethrough = true,
-      invert_selection = false,
-      invert_signs = false,
-      invert_tabline = false,
-      inverse = true,
-      contrast = 'soft',
-      palette_overrides = {},
-      overrides = {},
-      dim_inactive = false,
-      transparent_mode = false,
-    }
-    vim.cmd.colorscheme 'gruvbox'
-    vim.cmd.hi 'Comment gui=none'
-    vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none', fg = '#ebdbb2' })
+  config = function()
+    require('github-monochrome').setup {}
+    vim.cmd.colorscheme 'github-monochrome-solarized'
+    -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+    -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   end,
 }
