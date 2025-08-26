1877207ea63e964036c45a97c8e37d2a6322d6e0
amritxyz <amrit44404@proton.me>
2025-08-26 16:50:48 +0545

    using own custom colorscheme "solarized-dark"

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ require 'config.keymap'
 require 'config.undercurl'
 require 'config.api'
 require 'config.extras'
+require 'colors.solarized-dark'
 
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
