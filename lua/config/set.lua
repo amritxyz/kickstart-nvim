55ae37a43deb5cbd27d0ef8db6fa8e9975362cf6
amritxyz <amrit44404@proton.me>
2025-09-19 01:24:55 +0545

    feat: more usable netrw

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -37,6 +37,25 @@ vim.opt.smartindent = true
 
 vim.highlight.priorities.semantic_tokens = 95
 
+-- netrw config
+vim.g.netrw_banner = 0 -- gets rid of the annoying banner for netrw
+vim.g.netrw_browse_split = 4
+vim.g.netrw_altv = 0 -- change from left splitting to right splitting
+vim.g.netrw_liststyle = 3 -- tree style view in netrw
+vim.g.netrw_winsize = 70 -- takes up 70% of the screen
+vim.g.netrw_sizestyle = 'H'
+vim.g.netrw_sort_sequence = [[[\/]$,*]] -- sort directories first
+vim.g.netrw_keepdir = 0
+vim.g.netrw_sort_sequence = [[[\/]$,*]]
+vim.g.netrw_sizestyle = 'H'
+vim.g.netrw_localcopydircmd = 'cp -r'
+vim.g.netrw_localmkdir = 'mkdir -p'
+vim.g.netrw_localrmdir = 'rm -r'
+vim.g.netrw_compress = 'gzip'
+vim.g.netrw_cursor = 2
+vim.g.netrw_preview = 0
+vim.g.netrw_alto = 1
+
 -- vim.cmd [[autocmd BufNewFile  *.jsx 0r $HOME/.config/nvim/snippets/auto-load/jsx.jsx|normal Gddgg0fC]]
 
 -- vim.opt.tabstop = 8
